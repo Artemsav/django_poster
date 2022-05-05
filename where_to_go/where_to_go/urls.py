1d15724ec5c0f9db89c067dea0b4e5a77c0c9d8a
artemsav <studentbedn@gmail.com>
2022-05-05 15:42:58 +0300

    Add blank start page

--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import py_compile
 from django.contrib import admin
 from django.urls import path
+from where_to_go import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', view.index)
 ]
