f981ee03e84f07b99ef2d4f4971232580816f146
artemsav <studentbedn@gmail.com>
2022-05-06 13:58:48 +0300

    create places app

---
+++ where_to_go/places_to_go/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlacesToGoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'places_to_go'
--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!)vx-2#e&e6aq^jyn)&514+96769)@yh2*=p!(baqwq-6qcm(l'
+SECRET_KEY = os.getenv('SECRET_KEY', 'REPLACE_ME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'places_to_go.apps.PlacesToGoConfig',
 ]
 
 MIDDLEWARE = [
