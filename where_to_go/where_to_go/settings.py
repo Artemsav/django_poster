2d98a73d520eaa81bd4c95065151de5987954472
artemsav <studentbedn@gmail.com>
2022-05-26 11:39:12 +0300

    correct settings, add loading env to wsgi.py

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'REPLACE_ME')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*')
 
 
 # Application definition
@@ -125,7 +123,10 @@ USE_TZ = True
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
-STATIC_ROOT = (os.path.join(BASE_DIR, "static"))
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- where_to_go/where_to_go/wsgi.py
+++ where_to_go/where_to_go/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dotenv import load_dotenv
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'where_to_go.settings')
 
 application = get_wsgi_application()
+
+project_folder = os.path.expanduser('~/hisp.pythonanywhere.com')
+load_dotenv(os.path.join(project_folder, '.env'))
